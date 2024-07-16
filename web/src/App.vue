de9de95fc3687b0fb283cfd6ae49f30c7d3cac15
Dante <2802471732@qq.com>
2024-07-16 09:35:27 +0800

    新增导航组件Navbar...

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,11 @@
 <template>
+  <Navbar/>
   <router-view/>
 </template>
+
+<script setup>
+import Navbar from '@/components/Navbar'
+</script>
 
 <style lang="scss">
 *{
---
+++ web/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="navbar">
+        <el-menu :default-active="route.fullPath" class="el-menu-demo" mode="horizontal" :router="true" @select="handleSelect">
+            <el-menu-item index="/">首页</el-menu-item>
+            <el-menu-item index="/news">新闻中心</el-menu-item>
+            <el-menu-item index="/product">产品与服务</el-menu-item>
+            <el-menu-item index="" @click="handleClick">登录</el-menu-item>
+        </el-menu>
+        <div class="right">百院门户官网</div>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const handleClick=()=>{
+    window.location='http://localhost:8081/'
+}
+</script>
+
+<style scoped   lang="scss">
+.navbar{
+    position:sticky;
+    top:0;
+    z-index: 990;   
+}
+.right{
+    position: fixed;
+    right: 11px;
+    top: 0;
+    // widows: 100px;
+    // height: 159px;
+    line-height: 139px;
+    text-align: center;
+}
+</style>
