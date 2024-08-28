12d0140125a5b8bc60b50679e07c427fc3d3bfee
Dante <2802471732@qq.com>
2024-08-28 13:31:28 +0800

    增加微信小程序端获取新闻列表服务请求...

--- servers/router_handler/webnews.js
+++ servers/router_handler/webnews.js
@@ -1,29 +1,49 @@
 const db = require('../db/index');
 
 const WebNewsHandler = {
+    // 获取新闻列表
     list: (req, res) => {
-        if (req.params.id) {
-            const sqlStr = 'select * from news where id=? and isPublish = "1" ORDER BY editTime DESC '
-            db.query(sqlStr, req.params.id, (err, results) => {
-                if (err || results.length !== 1) return res.send({
-                    ActionType: "Error",
-                    message: '获取新闻列表失败' + err.message
-                })
-                return res.send({
-                    ActionType: "OK",
-                    message: '获取新闻成功',
-                    data: results[0]
-                })
-            })
-        } else {
-            const sqlStr = 'SELECT * FROM news WHERE isPublish = "1" ORDER BY editTime DESC'
-            db.query(sqlStr, (err, results) => {
+        
+        // 小程序端分类type------------
+        const { type } = req.query
+        const category =Number(type)+1
+        // ----------------------------
+
+        if (type) {
+            // 小程序端请求
+            const sqlStr = 'SELECT * FROM news WHERE isPublish = "1" AND category = ?  ORDER BY editTime DESC'
+            db.query(sqlStr, [category], (err, results) => {
                 if (err) return res.send({ ActionType: "Error", message: '列表获取失败' + err.message })
                 res.send({ ActionType: "OK", data: results, message: '获取新闻列表成功' })
             })
+        } else {
+            // WEB端请求
+            if (req.params.id) {
+                const sqlStr = 'select * from news where id=? and isPublish = "1" ORDER BY editTime DESC '
+                db.query(sqlStr, req.params.id, (err, results) => {
+                    if (err || results.length !== 1) return res.send({
+                        ActionType: "Error",
+                        message: '获取新闻列表失败' + err.message
+                    })
+                    return res.send({
+                        ActionType: "OK",
+                        message: '获取新闻成功',
+                        data: results[0]
+                    })
+                })
+            } else {
+                const sqlStr = 'SELECT * FROM news WHERE isPublish = "1" ORDER BY editTime DESC'
+                db.query(sqlStr, (err, results) => {
+                    if (err) return res.send({ ActionType: "Error", message: '列表获取失败' + err.message })
+                    res.send({ ActionType: "OK", data: results, message: '获取新闻列表成功' })
+                })
+            }
         }
+
+
     },
 
+    // 获取最新四条新闻列表
     toplist: (req, res) => {
         // Get query parameter
         const { limit } = req.query;
